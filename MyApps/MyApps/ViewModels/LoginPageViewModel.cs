16b9c1af856cd09c92a68d0f7d71983a6bf5513c
rico.putra <rico.putra@outlook.co.id>
2019-06-23 11:53:01 +0700

    Initiate MyApps with LoginPage has been created

---
+++ MyApps/MyApps/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApps.ViewModels
+{
+	public class LoginPageViewModel : BindableBase
+	{
+        public string Username { get; set; } 
+        public string Password { get; set; }
+
+        public LoginPageViewModel()
+        {
+
+        }
+	}
+}
---
+++ MyApps/MyApps/Views/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace MyApps.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            LogoSource.Source = ImageSource.FromResource("MyApps.Icons.logo.png");
+        }
+    }
+}
